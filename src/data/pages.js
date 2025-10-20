d9370a88e1a4ea9cb7aa05cbd6b7593131f0fc2a
Univercraft <160036881+Univercraft@users.noreply.github.com>
2025-10-20 12:08:35 +0200

    Config image

--- src/data/pages.js
+++ src/data/pages.js
@@ -2,7 +2,7 @@
 
 export const pages = [
   // 1 - couverture début
-  { id: 1, type: "cover", img: "/public/assets/page_1.png", title: "Les trois cheveux d’or du diable", text: "" },
+  { id: 1, type: "cover", img: "./public/assets/page_1.png", title: "Les trois cheveux d’or du diable", text: "" },
 
   // 2 - présentation
   { id: 2, type: "presentation", img: "/public/assets/page_2.png", title: "Les trois cheveux d’or du diable", translator: "Frédéric Baudry", author: "Les frères Grimm" 
