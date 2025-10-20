ac6f83899f8175b7d518f5868db6c5f980bbbfd0
Univercraft <160036881+Univercraft@users.noreply.github.com>
2025-10-20 11:41:24 +0200

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    tailwindcss(), react()
+    tailwindcss(),
   ],
   // Remplacez 'nom-du-repo' par le nom de votre repository GitHub
   base: process.env.NODE_ENV === 'production' ? 'SAE_501_Axel' : '/'
